2bfb33efeae155053407a9956850d22ff13f97e6
Reece Dunham <me@rdil.rocks>
2019-11-26 23:46:14 +0000

    init commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="intutils",
+    version="1.0.0",
+    author="Reece Dunham",
+    author_email="me@rdil.rocks",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    url="https://github.com/rdilweb/intutils",
+    description="Basic integer manipulation library.",
+    python_requires=">3.3",
+    classifiers=[
+        "int",
+        "integer",
+        "utils",
+        "util",
+        "utilities"
+    ]
+)
