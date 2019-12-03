7e924378a2ff7dee041cf4c0c7660ec8af1c0c38
Reece Dunham <me@rdil.rocks>
2019-12-03 15:25:39 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@ import setuptools
 
 setuptools.setup(
     name="intutils",
-    version="1.0.0",
+    version="1.1.0",
     author="Reece Dunham",
     author_email="me@rdil.rocks",
+    license="MIT",
     packages=setuptools.find_packages(),
     include_package_data=True,
     url="https://github.com/rdilweb/intutils",
     description="Basic integer manipulation library.",
     python_requires=">3.3",
+    project_urls={
+        "Documentation": "https://docs.rdil.rocks/libraries/intutils"
+    },
     keywords=[
         "int",
         "integer",
